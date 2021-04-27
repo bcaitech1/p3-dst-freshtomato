95b8551fefe3a0f9748bbae00556cff3d52d3d4f
iloveslowfood <silkstaff@naver.com>
2021-04-27 04:43:18 +0000

    add dirs for train/eval

---
+++ config.py
@@ -0,0 +1,7 @@
+class CFG:
+    TrainSlotMeta: str = './input/data/train_dataset/slot_meta.json'
+    TrainOntology: str = './input/data/train_dataset/ontology.json'
+    TrainDials: str = './input/data/train_dataset/train_dials.json'
+
+    EvalSlotMeta: str = './input/data/eval_dataset/slot_meta.json'
+    EvalDials: str = './input/data/eval_dataset/train_dials.json'
