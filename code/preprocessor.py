6f766c867faaa7da35882692d5cb58e6ab99c4ab
iloveslowfood <silkstaff@naver.com>
2021-04-29 05:09:54 +0000

    refactor convert_examples_to_features: apply tqdm by change representation from map to list comprehension

--- code/preprocessor.py
+++ code/preprocessor.py
@@ -1,5 +1,7 @@
+from typing import List
+from tqdm import tqdm
 import torch
-from data_utils import DSTPreprocessor, OpenVocabDSTFeature, convert_state_dict
+from data_utils import DSTPreprocessor, OpenVocabDSTFeature, convert_state_dict, DSTInputExample
 
 
 class TRADEPreprocessor(DSTPreprocessor):
@@ -19,7 +21,19 @@ class TRADEPreprocessor(DSTPreprocessor):
         self.id2gating = {v: k for k, v in self.gating2id.items()}
         self.max_seq_length = max_seq_length
 
-    def _convert_example_to_feature(self, example):
+    def _convert_example_to_feature(self, example: DSTInputExample) -> OpenVocabDSTFeature:
+        """List[DSTInputExample]를 feature로 변형하는 데 사용되는 nested 함수. 다음과 같이 사용
+            
+            # examples: List[DSTInputExample]
+            processor = TRADEPreprocessor(slot_meta, tokenizer)
+            features = processor.convert_examples_to_features(examples)
+
+        Args:
+            example (DSTInputExample)
+
+        Returns:
+            [OpenVocabDSTFeature]: feature 데이터
+        """        
         dialogue_context = " [SEP] ".join(example.context_turns + example.current_turn)
 
         input_id = self.src_tokenizer.encode(dialogue_context, add_special_tokens=False)
@@ -49,8 +63,17 @@ class TRADEPreprocessor(DSTPreprocessor):
             example.guid, input_id, segment_id, gating_id, target_ids
         )
 
-    def convert_examples_to_features(self, examples):
-        return list(map(self._convert_example_to_feature, examples))
+    def convert_examples_to_features(self, examples: List[DSTInputExample]) -> List[OpenVocabDSTFeature]:
+        """복수의 DSTInputExmple 각각을 feature로 변형하는 함수
+
+        Args:
+            examples (List[DSTInputExample]): DSTInputExample로 구성된 리스트
+
+        Returns:
+            List[OpenVocabDSTFeature]: feature로 변형된 데이터의 리스트
+        """
+        features = [self._convert_example_to_feature(e) for e in examples]
+        return features
 
     def recover_state(self, gate_list, gen_list):
         assert len(gate_list) == len(self.slot_meta)
